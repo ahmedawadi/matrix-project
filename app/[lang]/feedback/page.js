36895935a5de93763c162b6e3a9e8571586acffc
ahmedawadi <110494883+ahmedawadi@users.noreply.github.com>
2024-03-05 23:56:31 +0100

    chaning the ./feedback to ./feedback.js

--- app/[lang]/feedback/page.js
+++ app/[lang]/feedback/page.js
@@ -1,5 +1,5 @@
 import { getDictionary } from "../../../dictionnaries/dictionaries"
-import FeedBack from "./feedback"
+import FeedBack from "./feedback.js"
 
 export default async function Page({params}){
 
